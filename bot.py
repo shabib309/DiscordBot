33bf774c91ad77288b075598a29115394627d943
Maximilian Horn <horn@singhammer.com>
2021-02-23 00:45:49 +0100

    mhmmmm

--- bot.py
+++ bot.py
@@ -21,12 +21,12 @@ async def on_ready():
 
 
 async def botinfo(message):
-    clear_func_call(message)
+    await clear_func_call(message)
     await message.channel.send('Alive and happy :)')
 
 
 async def print_text(message, temp):
-    clear_func_call(message)
+    await clear_func_call(message)
     output = ""
     for x in temp[1:]:
         output += (x + " ")
@@ -34,7 +34,7 @@ async def print_text(message, temp):
 
 
 async def roulette(message):
-    clear_func_call(message)
+    await clear_func_call(message)
     result = random.randint(1, 6)
     if result == 1:
         await message.channel.send('U died...')
@@ -43,7 +43,7 @@ async def roulette(message):
 
 
 async def clear(message, temp):
-    clear_func_call(message)
+    await clear_func_call(message)
     if len(temp) == 1:
         await message.channel.purge(limit=1)
     else:
@@ -70,19 +70,19 @@ async def coinflip(message):
 
 
 async def help(message):
-    clear_func_call(message)
+    await clear_func_call(message)
     output = ""
     for x in options:
         output += x + "\n"
     await message.channel.send(output)
 
 async def clear_message(message, id):
-    clear_func_call(message)
+    await clear_func_call(message)
     msg = await message.channel.fetch_message(id)
     await msg.delete()
 
 async def embed(message, content):
-    clear_func_call(message)
+    await clear_func_call(message)
     output = ""
     for x in content[1:]:
         output += x + " "
@@ -91,7 +91,7 @@ async def embed(message, content):
     await message.channel.send(embed=embedVar)
 
 async def quote(message, content):
-    clear_func_call(message)
+    await clear_func_call(message)
     output = ""
     for x in content[1:]:
         output += x + " "
@@ -109,7 +109,6 @@ async def on_message(message):
         return
     temp = message.content.split()
     option = message.content.lower()
-    message_for_delete = message
     if option == '!botinfo':
         await botinfo(message)
     elif temp[0] == '!print':
