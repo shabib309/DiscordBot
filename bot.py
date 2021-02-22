77d6da75983f4c64f9047be6f1b816cb22113c43
Maximilian Horn <maxhorn62@gmail.com>
2021-02-22 16:44:22 +0100

    CoinFlip etc

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-# bot.py
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -14,20 +13,13 @@ client = commands.Bot(command_prefix='!')
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
-
-
-@client.command()
-async def clear(ctx, amount=5):
-    await ctx.message.delete()
-
-
-@client.command()
-async def kick(ctx, member: discord.member, *, reason="None"):
-    await member.kick(reason=reason)
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="ASMR"))
 
 
 @client.event
 async def on_message(message):
+    if message.author == client.user:
+        return
     temp = message.content.split()
     if message.content == '!botinfo':
         await message.channel.send('Alive and happy :)')
@@ -43,8 +35,23 @@ async def on_message(message):
         else:
             await message.channel.send('U survived...')
     elif message.content == '!help':
-        await message.channel.send("!botinfo\n!print\n!Russisch Roulette\n!help")
-    message.delete()
-    client.add_command(command=clear)
+        await message.channel.send("!help\n!botinfo\n!print\n!Russisch Roulette")
+    elif temp[0] == '!clear':
+        if len(temp) == 1:
+            await message.channel.purge(limit=1)
+        else:
+            await message.channel.purge(limit=(int(temp[1]) + 1))
+    elif message.content == '!CoinFlip':
+        await message.channel.send("Type \"Heads\" or \"Tails\"")
+        def check(m):
+            return (m.content.lower() == "heads" and m.channel == message.channel) or (m.content.lower() == "tails" and m.channel == message.channel)
+        msg = await client.wait_for("message", check=check)
+        result = random.randint(1,2)
+        if result == 1:
+            if msg.content.lower() == "tails":
+                await message.channel.purge(limit=1)
+                await message.channel.send("U won")
+            else:
+                await message.channel.send("U lost")
 
 client.run(TOKEN)
