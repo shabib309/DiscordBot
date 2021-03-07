b96867bb3db538eb3237ec8fd4f11b03e6f66f12
Maximilian Horn <horn@singhammer.com>
2021-03-07 06:25:31 +0100

    Fixed CoinFlip; Added Steam and Dog;now Init Daily

--- bot.py
+++ bot.py
@@ -7,17 +7,25 @@ import os
 import requests
 import re
 import translators as ts
+import json
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 NASA_TOKEN = os.getenv('NASA_TOKEN')
 WEATHER_TOKEN = os.getenv('WEATHER_TOKEN')
 
-options = {"!help", "!botinfo", "!nasa (Can only be started once) <channelID>", "!print <YourText>", "!donate", "!weather <cityName>", "!ip <ip-address>", "!translate <YourText>", "!pin <message_id>", "!joke", "!fuckoff", "!Russisch Roulette", "!CoinFlip", "!clear <quantity (limit = 10)>", "!clear_message <message_id>", "!embed <YourText>", "!quote <YourText>"}
+options = {"!help", "!botinfo", "!init_daily (Can only be started once) <channelID>", "!dog", "!steam <YourSteamLink>", "!print <YourText>", "!donate", "!weather <cityName>", "!ip <ip-address>", "!translate <YourText>", "!pin <message_id>", "!joke", "!fuckoff", "!Russisch Roulette", "!CoinFlip", "!clear <quantity (limit = 10)>", "!clear_message <message_id>", "!embed <YourText>", "!quote <YourText>"}
+options = sorted(options, key=str.lower)
 
 actions = ['awesome/', 'because/', 'bye/', 'cool/', 'diabetes/', 'everyone/', 'everything/', 'fascinating/', 'flying/', 'life/', 'pink/', 'thanks/', 'that/', 'this/', 'what/']
 
 client = commands.Bot(command_prefix='!')
+
+global daily_running, coinflip_running, delete_id, quote_id
+coinflip_running = False
+daily_running = False
+delete_id = 0
+quote_id = 0
 
 
 @client.event
@@ -61,19 +69,32 @@ async def clear(message, temp):
             return
         await message.channel.purge(limit=(int(temp[1])))
 
+async def coinflip_init(message):
+    await clear_func_call(message)
+    global coinflip_running
+    if coinflip_running:
+        await coinflip(message)
+    else:
+        msg = await message.channel.send("Type \"Heads\" or \"Tails\"")
+        global delete_id
+        delete_id = msg.id
+        coinflip_running = True
 
 async def coinflip(message):
-    await clear_func_call(message)
-    await message.channel.send("Type \"Heads\" or \"Tails\"")
-    def check(m):
-        return m.content.lower() == "heads" or m.content.lower() == "tails"
-    msg = await client.wait_for("message", check=check, timeout=10)
-    result = random.randint(1, 2)
-    if result == 1:
-        if msg.content.lower() == "tails":
+    result = random.randint(0, 1)
+    if result == 0:
+        if message.content.lower() == "heads":
             await message.channel.send("U won")
         else:
             await message.channel.send("U lost")
+    else:
+        if message.content.lower() == "tails":
+            await message.channel.send("U won")
+        else:
+            await message.channel.send("U lost")
+    await clear_message(message, delete_id, False)
+    global coinflip_running
+    coinflip_running = False
 
 
 async def help(message):
@@ -84,8 +105,9 @@ async def help(message):
     await message.channel.send(output)
 
 
-async def clear_message(message, id):
-    await clear_func_call(message)
+async def clear_message(message, id, clear_call=True):
+    if clear_call:
+        await clear_func_call(message)
     msg = await message.channel.fetch_message(id)
     await msg.delete()
 
@@ -187,6 +209,32 @@ async def translate(message, text):
     result = ts.google(text, to_language='de')
     await message.channel.send(result)
 
+async def qotd(id):
+    global quote_id
+    channel = client.get_channel(int(id))
+    if quote_id != 0:
+        old_msg = await channel.fetch_message(quote_id)
+        await old_msg.delete()
+    out = ""
+    out = await request_call("https://quotes.rest/qod?language=en", "\"quote\": \".*\",", 10, -2)
+    msg = await channel.send(out)
+    quote_id = msg.id
+    while client.is_ready:
+        await asyncio.sleep(60 * 60 * 24)
+        await apod(id)
+
+async def steam(message, link):
+    await clear_func_call(message)
+    if "http" in link:
+        link = link[36:-1]
+    response = await request_call("https://steamsignature.com/sig/" + link + "/", "value=\"http://steamsignature.com/card/0/.*.png", 7 ,0)
+    await message.channel.send(response)
+
+async def dog(message):
+    await clear_func_call(message)
+    result = await request_call("https://random.dog/woof.json", "\"url\":.*\"}", 7, -2)
+    await message.channel.send(result)
+
 async def request_call(url="", search="", startOffset=0,endOffset=0):
     if url == "":
         return ""
@@ -194,15 +242,23 @@ async def request_call(url="", search="", startOffset=0,endOffset=0):
     if search != "":
         result = re.search(search, response)
     content = ""
-    if endOffset != 0:
-        content = result.group(0)[startOffset:endOffset]
-    else:
-        content = result.group(0)[startOffset:]
+    try:
+        if endOffset != 0:
+            content = result.group(0)[startOffset:endOffset]
+        else:
+            content = result.group(0)[startOffset:]
+    except:
+        return response
     return content
 
-async def init_apod(message, id):
+async def init_daily(message, id):
     await clear_func_call(message)
+    global daily_running
+    if daily_running:
+        return
     client.loop.create_task(apod(id))
+    client.loop.create_task(qotd(id))
+    daily_running = True
 
 async def clear_func_call(message_for_delete):
     await message_for_delete.channel.purge(limit=1)
@@ -226,9 +282,9 @@ async def on_message(message):
     elif temp[0] == '!clear':
         await clear(message, temp)
     elif temp[0] == '!clear_message' and temp[1] != 0:
-        await clear_message(message, temp[1])
-    elif option == '!coinflip':
-        await coinflip(message)
+        await clear_message(message, temp[1], True)
+    elif option == '!coinflip' or option == 'heads' or option == 'tails':
+        await coinflip_init(message)
     elif temp[0] == '!embed':
         await embed(message, temp)
     elif temp[0] == '!quote':
@@ -239,8 +295,8 @@ async def on_message(message):
         await fuckoff(message)
     elif temp[0] == '!pin':
         await pin(message, temp[1])
-    elif temp[0] == '!nasa' and (temp[1] != 0 or temp[1] == 'stop'):
-        await init_apod(message, temp[1])  
+    elif temp[0] == '!init_daily' and temp[1] != 0:
+        await init_daily(message, temp[1])  
     elif option == '!donate':
         await donate(message)
     elif temp[0] == '!ip' and temp[1] != 0:
@@ -249,5 +305,9 @@ async def on_message(message):
         await weather(message, temp[1])
     elif temp[0] == '!translate' and temp[1] != 0:
         await translate(message, temp)
+    elif temp[0] == '!steam' and temp[1] != "":
+        await steam(message, temp[1])
+    elif option == '!dog':
+        await dog(message)
 
-client.run(TOKEN)
+client.run("ODEzMTY1NTcxNzA0NjE5MDI4.YDLVdA.1tRdRpiecI-25zbKj-W2FpQ5xWU")
